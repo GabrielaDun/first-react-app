b5ddbe358f2b2c35436f70d80ca167916e8919eb
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-13 12:05:00 +0200

    Added icones to the Lists

--- first-react-app/src/components/Column/Column.js
+++ first-react-app/src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.module.scss';
 const Column = props => {
     return (
         <article className={styles.column}>
-            <h2 className={styles.title} title ={props.title}> {props.title} </h2>
+            <h2 className={styles.title} title ={props.title}><span className={styles.icon + ' fa fa-' + props.icon}> <i className={props.icon}></i></span> {props.title} </h2>
         </article>
     );
   };
