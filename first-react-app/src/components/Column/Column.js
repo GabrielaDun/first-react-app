2ed05d6d7e89140de42c75220803eeedc1784ab0
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-14 14:35:19 +0200

    Changed elements into Usestate hooks

--- first-react-app/src/components/Column/Column.js
+++ first-react-app/src/components/Column/Column.js
@@ -4,6 +4,7 @@ const Column = props => {
     return (
         <article className={styles.column}>
             <h2 className={styles.title} title ={props.title}><span className={styles.icon + ' fa fa-' + props.icon}> <i className={props.icon}></i></span> {props.title} </h2>
+            
         </article>
     );
   };
--- first-react-app/src/components/List/List.js
+++ first-react-app/src/components/List/List.js
@@ -1,7 +1,14 @@
 import Column from '../Column/Column';
 import styles from './List.module.scss';
+import { useState } from 'react';
 
 const List = () => {
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
     return (
       <div>
           <header className={styles.header}>
@@ -9,10 +16,12 @@ const List = () => {
           </header>
           <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-              <Column title = "Books" icon = "book" />
-              <Column title = "Movies" icon = "film" />
-              <Column title = "Games" icon = "gamepad" />
+            {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
           </section>
+          <form>
+            <input type="text" />
+            <button>Add column</button>
+            </form>
       </div>
     );
   };
