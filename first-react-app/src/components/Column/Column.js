9af55756dee420fc0f04eecd9a0332bf0c5999a2
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-15 12:30:59 +0200

    Added Card components

--- first-react-app/src/components/Column/Column.js
+++ first-react-app/src/components/Column/Column.js
@@ -1,10 +1,13 @@
 import styles from './Column.module.scss';
+import Card from '../Card/Card';
 
 const Column = props => {
     return (
         <article className={styles.column}>
             <h2 className={styles.title} title ={props.title}><span className={styles.icon + ' fa fa-' + props.icon}> <i className={props.icon}></i></span> {props.title} </h2>
-            
+            <ul className={styles.cards}>
+                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
         </article>
     );
   };
