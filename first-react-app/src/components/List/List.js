ac4f8f1128696374e364dc13e3e6f1b46239453c
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-15 11:58:20 +0200

    Created ColumnForm component

--- first-react-app/src/components/List/List.js
+++ first-react-app/src/components/List/List.js
@@ -2,6 +2,7 @@ import Column from '../Column/Column';
 import styles from './List.module.scss';
 import { useState } from 'react';
 import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm';
 
 const List = () => {
     const [columns, setColumns] = useState([
@@ -9,12 +10,9 @@ const List = () => {
         { id: 2, title: 'Movies', icon: 'film' },
         { id: 3, title: 'Games', icon: 'gamepad' }
     ]);
-    const [value, setValue] = useState('');
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        setColumns([...columns, { id: shortid() , title: value }]);
-        setValue('');
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid() , title: newColumn.title, icon: newColumn.icon }]);
     };
     return (
       <div>
@@ -25,10 +23,7 @@ const List = () => {
           <section className={styles.columns}>
             {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
           </section>
-          <form onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <button>Add column</button>
-            </form>
+          <ColumnForm action={addColumn} />
       </div>
     );
   };
