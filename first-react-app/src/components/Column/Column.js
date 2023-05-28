5fc56c8e78a179210a4f8aef77785eaa429dcc93
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-28 07:32:11 +0200

    Change the app so columns and cards info are taken from a redux store

--- first-react-app/src/components/Column/Column.js
+++ first-react-app/src/components/Column/Column.js
@@ -1,9 +1,10 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
-//import shortid from 'shortid';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
     return (
         <article className={styles.column}>
             <h2 className={styles.title} title ={props.title}>
@@ -13,7 +14,7 @@ const Column = props => {
                 {props.title} 
             </h2>
             <ul className={styles.cards}> 
-                {props.cards.map(card => 
+                {cards.map(card => 
                 <Card 
                     key={card.id} 
                     title={card.title} 
--- first-react-app/src/components/List/List.js
+++ first-react-app/src/components/List/List.js
@@ -1,11 +1,13 @@
 import Column from '../Column/Column';
 import styles from './List.module.scss';
-import { useState } from 'react';
+//import { useState } from 'react';
 import shortid from 'shortid';
 import ColumnForm from '../ColumnForm/ColumnForm';
+import { useSelector } from 'react-redux';
+
 
 const List = () => {
-    const [columns, setColumns] = useState([
+    /*const [columns, setColumns] = useState([
         {
             id: 1,
             title: 'Books',
@@ -33,9 +35,9 @@ const List = () => {
                 { id: 2, title: 'Skyrim' }
             ]
         }
-    ]);
-
-    const addColumn = newColumn => {
+    ]);*/
+    
+    /*const addColumn = newColumn => {
         setColumns([...columns, 
             { id: shortid() , 
             title: newColumn.title, 
@@ -54,30 +56,26 @@ const List = () => {
         })
         setColumns(columnsUpdated);
     };
+    */
+    
 
+    const columns = useSelector(state => state.columns);
     return (
-      <div>
+        <div className={styles.list}>
           <header className={styles.header}>
-              <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+            <h2 className={styles.title}>Things to do<span>soon!</span></h2>
           </header>
-          <p className={styles.description}>
-            Interesting things I want to check out
-          </p>
+          <p className={styles.description}>Interesting things I want to check out</p>
           <section className={styles.columns}>
-            {columns.map(column => 
-                <Column 
-                key={column.id} 
-                id={column.id} 
-                title={column.title} 
-                icon={column.icon} 
-                cards={column.cards} 
-                action={addCards} 
-                />
+            {columns.map(column =>
+              <Column
+                key={column.id}
+                {...column}  />
             )}
           </section>
-          <ColumnForm action={addColumn} />
-      </div>
-    );
+          <ColumnForm />
+        </div>
+      );
   };
   
   export default List;
