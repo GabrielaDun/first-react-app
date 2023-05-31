f970f350ed5fdbbc0db4aaf8e8f98bdb9aed9ff7
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-05-31 12:10:09 +0200

    Added new sub-pages - About and Favorite

---
+++ first-react-app/src/components/About/About.js
@@ -0,0 +1,12 @@
+import styles from './About.module.scss'
+
+const About = () => {
+    return (
+        <div className={styles.about}>
+            <PageTitle>ABOUT</PageTitle>
+        </div>
+    )
+
+}
+
+export default About;
---
+++ first-react-app/src/components/Favorite/Favorite.js
@@ -0,0 +1,12 @@
+import styles from './Favorite.module.scss'
+
+const Favorite= () => {
+    return (
+        <div className={styles.favorite}>
+            <PageTitle>MY FIRST REACT APP</PageTitle>
+        </div>
+    )
+
+}
+
+export default Favorite;
